0404a2ae0b8621ae2e393b810db4937e7fc51ffc
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-09-21 20:52:32 +0700

    Add tutorial 14 and 15

---
+++ odinProjectTutorial15/js/themeChange.js
@@ -0,0 +1,11 @@
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+
+    document.querySelector('.theme-name').textContent = newTheme;
+}
+
+setTheme();
+document.querySelector('.theme-toggle').addEventListener('click', setTheme);
