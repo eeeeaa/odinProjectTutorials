f4d821d8b6e1b3380d870b7959f0aff70b8fb496
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-10-20 20:32:09 +0700

    add js tutorial 3

---
+++ Javascript/odinJSTutorial03/js/class.js
@@ -0,0 +1,61 @@
+class Clock {
+    constructor({ template }) {
+        this.template = template;
+    }
+
+    render = () => {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop = () => {
+        clearInterval(this.timer);
+    }
+
+    start = () => {
+        this.render();
+        this.timer = setInterval(this.render, 1000);
+    }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+//clock.start();
+
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a noise.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name) {
+      super(name); // call the super class constructor and pass in the name parameter
+    }
+  
+    speak() {
+      super.speak(); //can use super to call parent's method as well
+      console.log(`${this.name} barks.`);
+    }
+  }
+  
+  const d = new Dog("Mitzie");
+  d.speak();
